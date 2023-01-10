679b48c284ee065dc48373d2c57c600968ebf99b
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-01-10 11:20:36 -0400

    Add: created entity address

---
+++ src/Entities/addresses.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("addresses")
+class Address {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 8 })
+    zipCode: number;
+
+    @Column()
+    district: string;
+
+    @Column()
+    number: number;
+
+    @Column({ nullable: true })
+    reference: string;
+
+    @Column()
+    city: string;
+
+    @Column({ length: 2 })
+    state: string;
+};
+
+export default Address;
