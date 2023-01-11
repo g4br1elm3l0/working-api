238debbb62fb9c68fbd938adfb18cbf478e21491
Filipe Lucena <filipelucena1@gmail.com>
2023-01-11 13:09:58 -0300

    Fix: resolving conflicts on pull of Feature/Workers

--- src/Services/Workers/listWorker.service.ts
+++ src/Services/Workers/listWorker.service.ts
@@ -1,22 +1,12 @@
 import dataSource from "../../data-source";
-<<<<<<< HEAD
-import Workers from "../../Entities/workers.entity";
-
-
-const listWorkersService = async () => {
-    const workersRepo = dataSource.getRepository(Workers)
-
-    const workers = await workersRepo.find()
-=======
 import Users from "../../Entities/users.entity";
 
 const listWorkersService = async () => {
     const workersRepo = dataSource.getRepository(Users)
 
-    const workers = await workersRepo.find({
+    const workers = await workersRepo.findBy({
         isWorker: true
     })
->>>>>>> d762fde47418e367cbdea225e743a691e39ee3d4
 
     return workers
 }
