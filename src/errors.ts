c8db3a9977753bcb3ad005604cb28297ce18b62b
Eduardo.henrii <eduparaujo@outlook.com>
2023-01-11 11:14:49 -0300

    refatoracao service workers

--- src/errors.ts
+++ src/errors.ts
@@ -12,7 +12,6 @@ export default class AppError extends Error{
 
 export const handleError = async (err: Error, req: Request, res: Response, next: NextFunction) => {
 
-    console.log(err)
     if (err instanceof AppError){
         return res.status(err.statusCode).json({ message: err.message})
     }
