f8c5f5f9a0dfe27bbcf5686dd57f96e216504c00
gabriel <gabrielmelo98@hotmail.com>
2023-01-11 14:12:27 -0300

    list users

--- src/Controllers/users.controllers.ts
+++ src/Controllers/users.controllers.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express';
 import { createUserService } from '../Services/Users/createUser.service';
 import { IUsersRequest } from './../Interfaces/Users/index';
+import { listUserService } from './../Services/Users/listUser.service';
+
 export const createUserController = async (req: Request, res: Response) => {
     const userData: IUsersRequest = req.body
     const newUser = await createUserService(userData)
     const { password, ...myUser } = newUser
     return res.status(201).json(myUser);
 };
+
+export const listUserController = async (req: Request, res: Response) => {
+    const users = await listUserService()
+    return res.status(200).json(users)
+}
+
--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUserController } from './../Controllers/users.controllers';
+import { createUserController, listUserController } from './../Controllers/users.controllers';
 const userRouter = Router()
 
 
 userRouter.post('', createUserController)
+userRouter.get('', listUserController)
 
 
 export default userRouter
