9905372d12cac1242b30ba3ee5fa617a7625316d
Leonardo Miranda do Nascimento <leonardo.miranda.3@hotmail.com>
2023-01-11 16:30:45 -0400

    Add: test list users

--- src/__tests__/Integration/User/create.route.test.ts
+++ src/__tests__/Integration/User/create.route.test.ts
@@ -2,11 +2,10 @@ import request from "supertest";
 import { DataSource } from "typeorm";
 import dataSource from "../../../data-source";
 import app from "../../../app";
-import { mockedUser } from "../../Mocks/Integration/user.mock";
+import { mockedUser, mockedUserLogin, mockedUserWorker } from "../../Mocks/Integration/user.mock";
 
 describe("/users", () => {
     let connection: DataSource;
-    const baseUrl: string = "/users";
 
     beforeAll(async () => {
        await dataSource.initialize()
@@ -19,38 +18,47 @@ describe("/users", () => {
     });
 
     test("POST /users - Must be able to create user", async () => {
-        const response = await request(app).post(baseUrl).send(mockedUser);
+        const response = await request(app).post("/users").send(mockedUser);
 
-        expect(response.body).toHaveProperty("id")
-        expect(response.body).toHaveProperty("name")
-        expect(response.body).toHaveProperty("email")
-        expect(response.body).not.toHaveProperty("password")
-        expect(response.body).toHaveProperty("gender")
-        expect(response.body).toHaveProperty("birthday")
-        expect(response.body).toHaveProperty("profileImg")
-        expect(response.body).toHaveProperty("telephone")
-        expect(response.body).toHaveProperty("isActive")
-        expect(response.body).toHaveProperty("isWorker")
-        expect(response.body).toHaveProperty("createdAt")
-        expect(response.body).toHaveProperty("updatedAt")
+        expect(response.body).toHaveProperty("id");;
+        expect(response.body).toHaveProperty("name");
+        expect(response.body).toHaveProperty("email");
+        expect(response.body).not.toHaveProperty("password");
+        expect(response.body).toHaveProperty("gender");
+        expect(response.body).toHaveProperty("birthday");
+        expect(response.body).toHaveProperty("profileImg");
+        expect(response.body).toHaveProperty("telephone");
+        expect(response.body).toHaveProperty("isActive");
+        expect(response.body).toHaveProperty("isWorker");
+        expect(response.body).toHaveProperty("createdAt");
+        expect(response.body).toHaveProperty("updatedAt");
         
-        expect(response.body.name).toEqual("leonardo")
-        expect(response.body.email).toEqual("leonardo@mail.com")
-        expect(response.body.birthday).toEqual("02/10/98")
-        expect(response.body.gender).toEqual("masculino")
-        expect(response.body.profileImg).toEqual("profileleo")
-        expect(response.body.telephone).toEqual("67999956325")
-        expect(response.body.isActive).toEqual(true)
-        expect(response.body.isWorker).toEqual(true)
+        expect(response.body.name).toEqual("leonardo");
+        expect(response.body.email).toEqual("leonardo@mail.com");
+        expect(response.body.birthday).toEqual("02/10/98");
+        expect(response.body.gender).toEqual("masculino");
+        expect(response.body.profileImg).toEqual("profileleo");
+        expect(response.body.telephone).toEqual("67999956325");
+        expect(response.body.isActive).toEqual(true);
+        expect(response.body.isWorker).toEqual(true);
 
-        expect(response.status).toBe(201)
+        expect(response.status).toBe(201);
     });
 
     test("POST /users - should not be able to create a user that already exists", async () => {
-        const response = await request(app).post(baseUrl).send(mockedUser);
+        const response = await request(app).post("/users").send(mockedUser);
 
-        expect(response.body).toHaveProperty("message")
-        expect(response.status).toBe(409)
+        expect(response.body).toHaveProperty("message");
+        expect(response.status).toBe(409);
+    });
+
+    test("GET /users - Must be able to list users", async () => {
+        await request(app).post("/users").send(mockedUserWorker);        
+        const userWorkerLoginResponse = await request(app).post("/login").send(mockedUserWorker);
+        const response = await request(app).get("/users").set("Authorization", `Bearer ${userWorkerLoginResponse.body.token}`);
+
+        expect(response.body).toHaveLength(2);
+        expect(response.body)[0].not.toHaveProperty("password");
     });
 
     
--- src/__tests__/Mocks/Integration/user.mock.ts
+++ src/__tests__/Mocks/Integration/user.mock.ts
@@ -1,4 +1,4 @@
-import { IUserRequest } from "../../../Interfaces/Users";
+import { IUserLogin, IUserRequest } from "../../../Interfaces/Users";
 
 export const mockedUser : IUserRequest = {
     name: "leonardo",
@@ -8,5 +8,26 @@ export const mockedUser : IUserRequest = {
     birthday: "02/10/98",
     profileImg: "profileleo",
     telephone: "67999956325",
+    isWorker: false
+};
+
+export const mockedUserWorker : IUserRequest = {
+    name: "Leonardo 123",
+    email: "leonardo.123@mail.com",
+    password: "leo1234",
+    gender: "masculino",
+    birthday: "03/10/98",
+    profileImg: "profileleo",
+    telephone: "67999956325",
     isWorker: true
+};
+
+export const mockedUserLogin : IUserLogin = {
+    email: "leonardo@mail.com",
+    password: "leo1234"
+};
+
+export const mockedUserWorkerLogin : IUserLogin = {
+    email: "leonardo.123@mail.com",
+    password: "leo1234"
 };
