785c773b2793685a07d7a1c2ceb6d61ba449863c
Filipe Lucena <filipelucena1@gmail.com>
2023-01-11 12:43:27 -0300

    create Middleware ensureIsValidData

---
+++ src/Interfaces/Users/index.ts
@@ -0,0 +1,36 @@
+export interface IUserRequest {
+    name: string
+    email: string
+    password: string
+    birthday: string
+    gender: string
+    profileImg: string
+    isWorker: boolean
+    telephone: string
+}
+
+export interface IUserResponse {
+    id: string
+    name: string
+    email: string
+    birthday: string
+    gender: string
+    profileImg?: string 
+    telephone: string
+    createdAt: Date
+    updatedAt: Date
+    isActive: boolean
+}
+
+export interface IUserLogin {
+    email: string
+    password: string
+}
+
+export interface IUserUpdate {
+    name?: string
+    email?: string
+    password?: string
+    telephone?: string
+    profileImg?: string 
+}
