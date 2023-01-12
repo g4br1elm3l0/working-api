5e4df722b20c22c86e780d4949f0bb643f92fc5b
Eduardo.henrii <eduparaujo@outlook.com>
2023-01-12 17:22:45 -0300

    add middleware, modified service

---
+++ src/Middlewares/ensureIsActive.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import dataSource from "../data-source";
+import Users from "../Entities/users.entity";
+import AppError from "../errors";
+
+const ensureIsActive = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id
+
+    const findUser = await dataSource.getRepository(Users).findOneBy({
+        id
+    })
+
+    if (!findUser.isActive) {
+        throw new AppError('User is not active', 403)
+    }
+
+    return next()
+}
+
+export default ensureIsActive;
