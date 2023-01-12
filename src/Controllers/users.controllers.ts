2d95090fbb3e442face8579b98b7f74b544b455b
Filipe Lucena <filipelucena1@gmail.com>
2023-01-12 11:34:06 -0300

    Feat: List a specific user

--- src/Controllers/users.controllers.ts
+++ src/Controllers/users.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { createUserService } from '../Services/Users/createUser.service';
+import { listAnUserService } from '../Services/Users/listAnUser.service';
 import listWorkersService from '../Services/Users/listWorker.service';
 import { updateUserService } from '../Services/Users/uptadeUser.service';
 import { IUserRequest, IUserUpdate } from './../Interfaces/Users/index';
@@ -22,6 +23,10 @@ export const listWorkersController = async (req: Request, res: Response) => {
     return res.status(200).json(users)
 }
 
+export const listAnUserController = async (req: Request, res: Response) => {
+    const users = await listAnUserService(req.params.id)
+    return res.status(200).json(users)
+}
 
 export const UpdateUserController = async (req: Request, res: Response) => {
     const userParamsId: string = req.params.id
--- src/Routes/users.routes.ts
+++ src/Routes/users.routes.ts
@@ -5,15 +5,14 @@ import ensureIsAdmMiddleware from "../Middlewares/ensureIsAdm.middleware";
 import ensureIsValidDataMiddleware from "../Middlewares/ensureIsValidData.middleware";
 import { ensureIsValidIdMiddleware } from "../Middlewares/ensureIsValidId.middleware";
 import { requestUsersSerializer, updatedUserSerializer } from "../Serializers/users.serializers";
-import { createUserController, listUsersController, listWorkersController, UpdateUserController } from './../Controllers/users.controllers';
-const userRouter = Router()
+import { createUserController, listAnUserController, listUsersController, listWorkersController, UpdateUserController } from './../Controllers/users.controllers';
 
+const userRouter = Router();
 
-userRouter.post('', ensureIsValidDataMiddleware(requestUsersSerializer), createUserController)
-userRouter.get('', ensureAuthMiddleware, ensureIsAdmMiddleware,listUsersController)
-userRouter.get('/workers', ensureAuthMiddleware, ensureIsAdmMiddleware,listWorkersController)
-userRouter.get('/:id', ensureAuthMiddleware, ensureIsAdmMiddleware, ensureIsValidIdMiddleware(Users), )
-userRouter.patch('/:id', ensureAuthMiddleware, ensureIsAdmMiddleware, ensureIsValidIdMiddleware(Users), ensureIsValidDataMiddleware(updatedUserSerializer), UpdateUserController)
+userRouter.post('', ensureIsValidDataMiddleware(requestUsersSerializer), createUserController);
+userRouter.get('', ensureAuthMiddleware, ensureIsAdmMiddleware,listUsersController);
+userRouter.get('/workers', ensureAuthMiddleware, ensureIsAdmMiddleware,listWorkersController);
+userRouter.get('/:id', ensureAuthMiddleware, ensureIsAdmMiddleware, ensureIsValidIdMiddleware(Users), listAnUserController);
+userRouter.patch('/:id', ensureAuthMiddleware, ensureIsAdmMiddleware, ensureIsValidIdMiddleware(Users), ensureIsValidDataMiddleware(updatedUserSerializer), UpdateUserController);
 
-
-export default userRouter
+export default userRouter;
