c87ca9c3c254bdf981fb18a8a3fcce4a58fd077b
Filipe Lucena <filipelucena1@gmail.com>
2023-01-13 20:02:39 -0300

    Fix: list a service with relations and add comment with serializer format

--- src/Services/UserServices/listUserServicesbyUserId.service.ts
+++ src/Services/UserServices/listUserServicesbyUserId.service.ts
@@ -3,13 +3,39 @@ import UserServices from "../../Entities/userServices.entity";
 import { IUserService } from "../../Interfaces/UserServices";
 
 
-export const listUserServicesbyUserIdService = async (userId: string): Promise<IUserService[]> => {
+export const listUserServicesbyUserIdService = async (userId: string): Promise<IUserService> => {
 
     const userServicesRepository = dataSource.getRepository(UserServices);
 
-     const SearchUserServiceByUserId = await userServicesRepository.findBy({
-        id: userId
+     const SearchUserServiceByUserId = await userServicesRepository.findOne({
+        where: {
+            id: userId
+        },
+        relations: {
+            user: true,
+            category: true
+        }
+        
     })
 
     return SearchUserServiceByUserId;
 }
+
+// {
+//     id: "5e133a8d-e823-41bf-bf9e-ad5969f03ca3",
+//     title: "Consertem meu PC",
+//     description: "Meu PC quebrou",
+//     status: "pendente",
+//     "createdAt": "2023-01-13T17:46:35.588Z",
+//     "user": {
+//         "id": "fbc2ce2f-faed-4261-a478-f1d56be8fd6e",
+//         "name": "teste2",
+//         "email": "teste2@kenzie.com",
+//         "profileImg": "oi",
+//         "telephone": "89976999943",
+//     },
+//     "category": {
+//         "id": "a173a604-6ebf-4ab3-a80f-9a38a88c2837",
+//         "name": "assistência técnica"
+//     }
+// }
